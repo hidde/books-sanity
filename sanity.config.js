9b063636a3f8a77a9c7efcac55b2f3c4e47b60c4
Hidde de Vries <hidde@hiddedevries.nl>
2025-02-10 14:32:30 +0100

    update to latest sanity

--- sanity.config.js
+++ sanity.config.js
@@ -17,14 +17,35 @@ const config = {
     deskTool({
       structure: (S) =>
       S.list()
-      .title('Manage contents')
+      .title('Browse and collect data')
         .items(
         [
+          S.listItem()
+            .title('Speciale lijstjes')
+            .child(
+              S.list()
+                .title('Speciale lijstjes')
+                .items([
+                  S.listItem().title('Concerten die komen').child(
+                     S.documentTypeList('concert')
+                      .title('Concerten')
+                      .filter('date > now()')
+                      .child(),
+                  ),
+                  S.listItem().title('Boeken die nog niet uit zijn').child(
+                     S.documentTypeList('book')
+                      .title('Books')
+                      .filter('dateFinished > now()')
+                      .child(),
+                  ),
+                ])
+            ),
+          S.divider(),
           ...S.documentTypeListItems().filter(
             item => !['artist', 'concertVenue', 'festival', 'grower', 'coffeeStore'].includes(item.getId())
           ),
         ]
-      )               
+      ),               
     }), 
     visionTool()
   ],
